c6da5e3111ba1ebb617e8943b749b5db61cd957d
Ruhollah Majdoddin <r.majdodin@gmail.com>
2024-05-25 09:08:41 +0200

    added comments

--- minbpe/length.py
+++ minbpe/length.py
@@ -67,59 +67,37 @@ class ContiguousSupersetTrie:
 
 class LengthTokenizer(RegexTokenizer):
     def _encode_chunk(self, text_bytes, vocab=None):
-        #called in training?
+        #in training vocab != None
         voc = vocab if vocab else self.vocab_rev
 
         n = len(text_bytes)
         dp = [float("inf") for _ in range(n)]
         backtrack = [[-1] * n for _ in range(n)]
 
-        # Check for single words in the vocabulary
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         if text_bytes[i:j+1] in voc:
-        #             dp[i][j] = 1
-
-        # Compute the dp values
-        # for length in range(2, n + 1):  # length of the substring
-        #     for i in range(n - length + 1):
-        #         j = i + length - 1
-        #         if dp[i][j] == 1:  # If the whole substring is a word, skip further processing
-        #             continue
-        #         for k in range(i, j):
-        #             if dp[i][j] > dp[i][k] + dp[k+1][j]:
-        #                 dp[i][j] = dp[i][k] + dp[k+1][j]
-        #                 backtrack[i][j] = k
-
-        # Populate the dp table
+        #At ith iteration, dp[j] is the size of optimal tokenization of text_bytes[:j+1], for 0 < j < i-1, and
+        #backtrack[j] is the index of the first byte of the last token of an optimal tokenization.
         for i in range(0, n):
             if text_bytes[:i+1] in voc:
                 dp[i] = 1
                 backtrack[i] = 0
                 continue
             for j in range(1, i+1):
-                # for token in voc:
-                # token_len = len(token)
                 if text_bytes[j:i+1] in voc:
                     assert dp[j-1] >= 1
                     if dp[i] > dp[j-1] + 1:
                         dp[i] = dp[j-1] + 1
                         backtrack[i] = j
 
-        # Reconstruct the tokens from the dp table
+        # Reconstruct the tokens from the backtrack table
         def reconstruct_tokens(i):
-            # if i > j:
-            #     return []
             if backtrack[i] == 0:
                 return [text_bytes[:i+1]]
             k = backtrack[i]
             return reconstruct_tokens(k-1) + [text_bytes[k:i+1]]
 
-        if vocab: #called in training, return the ids
-        #    if dp[n-1] != len(self._encode_chunk_old(text_bytes, vocab)):
-        #        xxx = 1
+        if vocab: #called in training, return the size of optimal tokenization
            return dp[n-1]
-        else:
+        else: #return an optimal tokenization
             return [voc[token] for token in reconstruct_tokens(n - 1)]
 
     def train(self, text, vocab_size, verbose=False):
@@ -130,36 +108,40 @@ class LengthTokenizer(RegexTokenizer):
         # input text preprocessing
         ids = [ch.encode("utf-8") for ch in text_chunks]
 
-        # create a vocabulary dictionary, the key is the token bytes, the value is the number of times it appears the text-chunks tokenizatoin
+        #The key is a token (as bstring), the value is the number of times the token appears in the  tokenizatoin of text-chunks
         alltoks = defaultdict(int)
         alltoks.update({bytes([idx]):0  for idx in range(256)})
 
-        #add each chunk and all its sublists to vocab
+        #add each chunk and all its sublists to alltoks
         for i, chunkid in enumerate(ids):
             for start in range(len(chunkid)):
                 for end in range(start + 1, len(chunkid) + 1):
                     alltoks[chunkid[start:end]] += 1
 
-        #pruning any token that has same freq as one of its supertoken
+        #pruning any token that has same freq as one of its supertokens
         toremove = set()
         for token in alltoks:
             for start in range(len(token)):
                 for end in range(start + 1, len(token) + 1):
                     if token[start:end] != token and alltoks[token[start:end]] == alltoks[token]:
                         toremove.add(token[start:end])
+
         #pruning tokens that have small frequency
+        #The constant is emperical, b'over' occured just 13 times in tokenizaton of talor swirt wiki article
         for token in alltoks:
-            if alltoks[token] <= 25:
+            if alltoks[token] <= 3 * max(1, len(text) / 180000):
                 toremove.add(token)
 
         for token in toremove:
             if len(token) > 1:
                 del alltoks[token]
 
-        #auxillary data structures for efficiency. because some chunks can be identical, we identify a chunk by its idx index
+        #Auxillary data structures for efficiency.
+        #supchunks maps each token to the set of all the text chunks that contain the token.
+        #subtkns maps each token to the set of all of its contagious subtokens
+        #Because the can be identical chunks, we identify a chunk by its idx index
         supchunks = defaultdict(set)
         subtkns = defaultdict(set)
-        #add each chunk and all its sublists to vocab
         for i, chunkid in enumerate(ids):
             for start in range(len(chunkid)):
                 for end in range(start + 1, len(chunkid) + 1):
@@ -167,20 +149,31 @@ class LengthTokenizer(RegexTokenizer):
                         continue
                     supchunks[chunkid[start:end]].add(i)
                     subtkns[i].add(chunkid[start:end])
-        #we do not need the frequencies anymore
+
+        #we do not need the values any more
         alltoks = set(alltoks.keys())
-        #initialize vocab bei all single byte tokens
+        #initialize vocab bei all single byte tokens. These will not be removed.
         vocab = set(bytes([idx])  for idx in range(256)) # int -> bytes
 
-        change, added = None, None
-        #utils holds sum of utilitis of a token, over all chuncks with that token
+        mut, added = None, None
+        #utility maps each token to its effect on the size of an optimal tokenization of all chunks:
+        # if the token is not in vocab: How much the size would increase if the token is added to the current vocab
+        # if the token is in vocab: How much the size would decrease if the token is removed from the current vocab
+
+        #utility maps each token, chunk pair to effect of token on the size of an optimal tokenization of the chunks:
+        # if token is None: the size of an optimal tokenization of the chunk. Otherwise
+        #  if the token is not in vocab: How much the size would increase if the token is added to the current vocab
+        #  if the token is in vocab: How much the size would decrease if the token is removed from the current vocab
         utility, utils  = defaultdict(int), defaultdict(int)
-        #add the token with most reduction in size of tokenization
+        #Greedy algorithm: iteratively, add the token with most utility to vocab, or removing the token with ...
         while len(vocab) < vocab_size:
-            #find those tokens that share a chunk with change (from prev iteration)
-            affected_chunks = supchunks[change] if change else set(range(len(ids)))
-            affected_tokens = (reduce(set.union, [subtkns[chunk] for chunk in affected_chunks]) if change else alltoks) - set(bytes([idx])  for idx in range(256))#- vocab
-            #update length of minimal tokenizations of each chunk, according to the current vocab
+            #find all chunks ....
+            affected_chunks = supchunks[mut] if mut else set(range(len(ids)))
+            #find those tokens that share a chunk with the mutation (from prev iteration). If first iteration, alltoks. exclude 1-byte tokens
+            affected_tokens = (reduce(set.union, [subtkns[chunk] for chunk in affected_chunks]) if mut else alltoks) - set(bytes([idx])  for idx in range(256))#- vocab
+            #update utitlity and utils according to the last mutation
+            #change of utility of each token after the last mutation, is the sum of change of its utility for each chunk
+            #enough to consider those chunks that contain the token of the last mutation (or all chunks if it's the first iteration), and all the tokens contained in those chunks.
             for chunkid in affected_chunks:
                 utils[(None, chunkid)] = self._encode_chunk(ids[chunkid], vocab)
             for token in (affected_tokens & vocab):
@@ -200,8 +193,10 @@ class LengthTokenizer(RegexTokenizer):
                     utility[token] += utils[(token, chunkid)]
                 vocab.remove(token)
 
-
-            #TODO: remove a token only if its utility has reduced since it was inserted.
+            #utility of a token can change by mutations.
+            #From the tokens in vocab that have less utility than the last added token,
+            #and do not share a chunk with the lass added token (because then utility of last addition depends of the token, and it should not be affected, to avoid add/remove loops)
+            #remove the one with least utility
             worst = None
             for token in vocab - set(bytes([idx])  for idx in range(256)):
                 if (utility[token] < utility[added]) and not (supchunks[added] & supchunks[token]):
@@ -210,13 +205,16 @@ class LengthTokenizer(RegexTokenizer):
                     worst = token
             if worst:
                 vocab.remove(worst)
-                change = worst
-                print(f"removed {change} utility:{utility[change]}")
+                mut = worst
+                if verbose:
+                    print(f"removed {mut} utility:{utility[mut]}")
             else:
+                #Add the token to vocab, that has most utility.
                 added = max((token for token in utility if token not in vocab), key=utility.get)
-                change = added
-                vocab.add(change)
-                print(f"added {change} utility:{utility[change]}")
+                mut = added
+                vocab.add(mut)
+                if verbose:
+                    print(f"added {mut} utility:{utility[mut]}")
 
         #vocab of id:token, and vocab_rev of token:id
         self.vocab = {i:token for i, token in enumerate(vocab)}
