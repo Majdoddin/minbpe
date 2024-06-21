8f90cc0cd2c835177f7a2dee9232b7452dc7972d
Ruhollah Majdoddin <r.majdodin@gmail.com>
2024-06-21 20:43:05 +0200

    cleanup

--- minbpe/__init__.py
+++ minbpe/__init__.py
@@ -1,5 +1,5 @@
 from .base import Tokenizer
 from .basic import BasicTokenizer
 from .regex import RegexTokenizer
-# from .gpt4 import GPT4Tokenizer
+from .gpt4 import GPT4Tokenizer
 from .greedy import GreedyTokenizer
